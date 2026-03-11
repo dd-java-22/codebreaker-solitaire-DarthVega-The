a4ca1838072a5a3f11571c49536ddf234f6ac141
Mark <mvega37@cnm.edu>
2026-03-11 10:45:05 -0600

    Implement game summary display with ViewModel and layout updates.
    - Added `SummaryViewModel` to manage game summary data and integrated it with `InProgressFragment`.
    - Created `item_game_summary.xml` layout for individual game summaries.
    - Updated `fragment_in_progress.xml` to include header and `RecyclerView` for summaries.
    - Enhanced `InProgressFragment` to observe and populate game summaries using ViewModel.
    - Added new string resources for game summary fields.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/app/controller/InProgressFragment.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/app/controller/InProgressFragment.java
@@ -1,24 +1,47 @@
 package edu.cnm.deepdive.codebreaker.app.controller;
 
 import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.lifecycle.ViewModelProvider;
 import edu.cnm.deepdive.codebreaker.app.R;
 import edu.cnm.deepdive.codebreaker.app.databinding.FragmentInProgressBinding;
+import edu.cnm.deepdive.codebreaker.app.viewmodel.SummaryViewModel;
 
 
 public class InProgressFragment extends Fragment {
 
-private FragmentInProgressBinding binding;
+  private FragmentInProgressBinding binding;
 
   @Override
   public View onCreateView(
-      LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+      @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
     binding = FragmentInProgressBinding.inflate(inflater, container, false);
-
+        // TODO: Attach adapter to recyclerview
     return binding.getRoot();
+  }
+
+
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    SummaryViewModel viewModel = new ViewModelProvider(requireActivity()).get(
+        SummaryViewModel.class);
+    viewModel
+        .getInProgressSummaries()
+        .observe(getViewLifecycleOwner(), (summaries) -> {
+          //TODO: populate recyclerview adapter with summaries
+        });
+  }
+
+  @Override
+  public void onDestroyView() {
+    binding = null;
+    super.onDestroyView();
   }
 }
